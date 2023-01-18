0b33e3d58947aa5dbd922ffb028e8df11420c271
BharathwajShankar <saialumnibharathwaj@gmail.com>
2023-01-18 13:09:11 +0530

    Revert "LR-305 Moving old sunbird.org site contents to Lern microsite"
    This reverts commit 536b6ddeb55f63568d05559f812c1d8ceeb68847.

--- notification/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of notification
- */
-
-export * from './lib/notification.service';
-export * from './lib/notification.component';
-export * from './lib/notification.module';
-export * from './lib/models';
